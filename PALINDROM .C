b667f621f2c4918ba5ade99680ccba479e4df025
jenish0307 <131126665+jenish0307@users.noreply.github.com>
2023-08-25 12:26:59 +0530

    Add files via upload

---
+++ PALINDROM .C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+   char s[100];
+   int i,n,c=0;
+
+   printf("Enter the string = ");
+   gets(s);
+   n=strlen(s);
+
+   for(i=0;i<2;i++)
+   {
+    if (s[i]==s[n-i-1])
+    c++;
+
+   }
+   if(c==i)
+        printf("string is palindrome");
+   else
+        printf("stringshtring is not palindromer");
+
+
+
+}
